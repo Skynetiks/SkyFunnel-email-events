98a36dc5d35ed6d9b61e05c5edee7d84ccb8b9f5
amang06 <aman@skynetiks.com>
2024-08-11 22:42:04 +0530

    remove env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,10 +8,6 @@ module.exports = {
 			env_production: {
 				NODE_ENV: "production",
 				PORT: 8000,
-				SQS_QUEUE_URL: "https://sqs.us-east-1.amazonaws.com/211125710904/SkyFunnel-Email-Queue",
-				S3_REGION: "us-east-1",
-				AWS_ACCESS_KEY_ID: "AKIATCKASWA4B2Y7DLSF",
-				AWS_SECRET_ACCESS_KEY: "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",
 				// Add other environment variables here
 			}
 		}
